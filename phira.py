14b80930096183df54f40072e47e457d4b302c87
364hao <129530250+364hao@users.noreply.github.com>
2024-09-21 08:50:21 +0800

    feat: support for phira-render

--- phira.py
+++ phira.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import shutil
 from zipfile import ZipFile
 from rich.progress import track
@@ -30,8 +31,19 @@ with open("difficulty.tsv", encoding="utf8") as f:
 
 for id, info in track(infos.items(), description = "Writing..."):
     for level in range(len(info["difficulty"])):
+        print(id, info, level)
         with ZipFile("phira/%s/%s-%s.pez" % (levels[level], id, levels[level]), "x") as pez:
             pez.writestr("info.txt", "#\nName: %s\nSong: %s.ogg\nPicture: %s.png\nChart: %s.json\nLevel: %s Lv.%s\nComposer: %s\nIllustrator: %s\nCharter: %s" % (info["Name"], id, id, id, levels[level], info["difficulty"][level], info["Composer"], info["Illustrator"], info["Chater"][level]))
             pez.write("Chart_%s/%s.0.json" % (levels[level], id), "%s.json" % id)
             pez.write("Illustration/%s.png" % id, "%s.png" % id)
             pez.write("music/%s.ogg" % id, "%s.ogg" % id)
+
+if len(sys.argv) > 1 and sys.argv[1] == '--rpe':
+    for id, info in track(infos.items(), description = "WritingRpe..."):
+        for level in range(len(info["difficulty"])):
+            print(id, info, level)
+            with ZipFile("phira/%s/%s-%s-rpe.pez" % (levels[level], id, levels[level]), "x") as pez:
+                pez.writestr("info.txt", "#\nName: %s\nSong: %s.ogg\nPicture: %s.png\nChart: %s.json\nLevel: %s Lv.%s\nComposer: %s\nIllustrator: %s\nCharter: %s" % (info["Name"], id, id, id, levels[level], info["difficulty"][level], info["Composer"], info["Illustrator"], info["Chater"][level]))
+                pez.write("Chart_%s/%s.0.json" % (levels[level], id), "%s.rpe.json" % id)
+                pez.write("Illustration/%s.png" % id, "%s.png" % id)
+                pez.write("music/%s.ogg" % id, "%s.ogg" % id)
