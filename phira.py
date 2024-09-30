1b81aaf4e7328ab86cb1640797f51039f486e050
364hao <129530250+364hao@users.noreply.github.com>
2024-10-01 07:31:59 +0800

    Update phira.py

--- phira.py
+++ phira.py
@@ -45,7 +45,7 @@ if len(sys.argv) > 1 and sys.argv[1] == '--phira':
         for level in range(len(info["difficulty"])):
             try:
                 with ZipFile("phira/%s/%s-%s(Phira ver.).pez" % (levels[level], id, levels[level]), "w") as pez:
-                    pez.writestr("info.txt", "#\nName: %s\nSong: %s.ogg\nPicture: %s.png\nChart: %s.json\nLevel: %s Lv.%s\nComposer: %s\nIllustrator: %s\nCharter: %s" % (info["Name"], id, id, id, levels[level], info["difficulty"][level], info["Composer"], info["Illustrator"], info["Chater"][level]))
+                    pez.writestr("info.txt", "#\nName: %s\nSong: %s.ogg\nPicture: %s.png\nChart: %s.rpe.official.json\nLevel: %s Lv.%s\nComposer: %s\nIllustrator: %s\nCharter: %s" % (info["Name"], id, id, id, levels[level], info["difficulty"][level], info["Composer"], info["Illustrator"], info["Chater"][level]))
                     pez.write("Chart_%s/%s.0.json" % (levels[level], id), "%s.rpe.official.json" % id)
                     pez.write("Illustration/%s.png" % id, "%s.png" % id)
                     pez.write("music/%s.ogg" % id, "%s.ogg" % id)
